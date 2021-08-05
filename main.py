0295b758428f4610af6cd1179aefecd87d21c424
LengSicong <39758764+jessiexiye@users.noreply.github.com>
2021-08-05 13:45:43 +0800

    Debug loss calculation

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ import random
 import numpy as np
 import torch.nn as nn
 from model import MSLELoss, MLPModel
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from tqdm import tqdm
+import copy
 
 def get_varlen_features_size(train_dataloader):
     max_entities = 0
@@ -37,10 +38,11 @@ def train(config):
     val_dataloader = DataLoader(val_dataset, batch_size=config.batch_size)
     test_dataloader = DataLoader(test_dataset, batch_size=config.batch_size)
 
-    # get_varlen_features_size(train_dataloader)
+    get_varlen_features_size(train_dataloader)
 
     if torch.cuda.is_available():
         config.device = 'cuda:0'
+        print(config.device)
     else:
         config.device = 'cpu'
 
@@ -53,60 +55,83 @@ def train(config):
     train_losses = []
     val_losses = []
 
+    best_val_loss = np.inf
+
     for epoch in range(config.epochs):
+        train_loss = 0.0
+        val_loss = 0.0
+        test_loss = 0.0
+
         for step, (train_features, train_entities, train_mentions, train_hashtags, train_urls, train_labels) in enumerate(tqdm(train_dataloader)):
         
             optimizer.zero_grad()
             outputs = model(train_features.to(config.device), train_entities.to(config.device), train_mentions.to(config.device), train_hashtags.to(config.device), train_urls.to(config.device))
-            train_loss = criterion(outputs.cpu(), train_labels.float())
-            train_loss.backward()
+            train_loss_batch = criterion(outputs.cpu(), train_labels.float())
+            train_loss_batch.backward()
             optimizer.step()
-        
-        val_loss = 0.0
+            train_loss += train_loss_batch * len(train_labels)
+
         with torch.no_grad():
             # TODO: evaluate on val and test dataset
             for step, (val_features, val_entities, val_mentions, val_hashtags, val_urls, val_labels) in enumerate(val_dataloader):
                 outputs = model(val_features.to(config.device), val_entities.to(config.device), val_mentions.to(config.device), val_hashtags.to(config.device), val_urls.to(config.device))
-                val_loss += criterion(outputs.cpu(), val_labels.float())
+                val_loss += criterion(outputs.cpu(), val_labels.float()) * len(val_labels)
+            print(len(train_dataset))
+            print('epoch: {}, train_loss: {}, val_loss: {}'.format(epoch, train_loss.item()/len(train_dataset), val_loss.item()/len(val_dataset)))
             
-        print('epoch: {}, train_loss: {}, val_loss: {}'.format(epoch, train_loss.item(), val_loss.item()))
+            msle = MSLELoss()
+            for step, (test_features, test_entities, test_mentions, test_hashtags, test_urls, test_labels) in enumerate(test_dataloader):
+                pred = model(test_features.to(config.device), test_entities.to(config.device), test_mentions.to(config.device), test_hashtags.to(config.device), test_urls.to(config.device))
+                test_loss += msle(pred.cpu(), test_labels.float()) * len(test_labels)
+            print('Test loss: ', str(test_loss/len(test_dataset)))
+
+        ### SAVE MODEL WITH THE LOWEST val_loss
+        if val_loss < best_val_loss:
+            best_val_loss = val_loss
+            torch.save(model.state_dict(), str(config.meanpooling)+'best_model.pth')
+
         train_losses.append(train_loss.item())
         val_losses.append(val_loss.item())
 
-    ### PLOT LOSSES ###
-    plt.plot(range(1, config.epochs+1), train_losses, 'g', label='Training loss')
-    plt.plot(range(1, config.epochs+1), val_losses, 'b', label='Validation loss')
-    plt.xlabel('Epochs')
-    plt.ylabel('Loss')
-    plt.legend()
-    plt.show()
+    # ### PLOT LOSSES ###
+    # plt.plot(range(1, config.epochs+1), train_losses, 'g', label='Training loss')
+    # plt.plot(range(1, config.epochs+1), val_losses, 'b', label='Validation loss')
+    # plt.xlabel('Epochs')
+    # plt.ylabel('Loss')
+    # plt.legend()
+    # plt.show()
 
     ### EVALUATE ON TEST SET ###
+    best_model = MLPModel(config)
+    best_model.load_state_dict(copy.deepcopy(torch.load(str(config.meanpooling)+'best_model.pth', torch.device(config.device))))
+    best_model.to(config.device)
     test_loss = 0.0
     with torch.no_grad():
         msle = MSLELoss()
         for step, (test_features, test_entities, test_mentions, test_hashtags, test_urls, test_labels) in enumerate(test_dataloader):
-            pred = model(test_features.to(config.device), test_entities.to(config.device), test_mentions.to(config.device), test_hashtags.to(config.device), test_urls.to(config.device))
-            test_loss += msle(pred.cpu(), test_labels.float())
-        print('Test loss: ', str(test_loss))
+            pred = best_model(test_features.to(config.device), test_entities.to(config.device), test_mentions.to(config.device), test_hashtags.to(config.device), test_urls.to(config.device))
+            test_loss += msle(pred.cpu(), test_labels.float()) * len(test_labels)
+        print('Test loss: ', str(test_loss/len(test_dataset)))
 
 # if __name__ == 'main':
 parser = ArgumentParser()
 parser.add_argument('--seed', type=int, default=2021)
-parser.add_argument('--batch_size', type=int, default=64)
+parser.add_argument('--batch_size', type=int, default=128)
 parser.add_argument('--epochs', type=int, default=100)
 parser.add_argument('--train_path', type=str, default='./data/train.feather')
 parser.add_argument('--val_path', type=str, default='./data/val.feather')
 parser.add_argument('--test_path', type=str, default='./data/test.feather')
-parser.add_argument('--lr', type=float, default=1e-4, help='learning rate')
+parser.add_argument('--lr', type=float, default=1e-3, help='learning rate')
 parser.add_argument('--dropout', type=float, default=0.5)
 parser.add_argument('--entities_dim', type=int, default=45677)
 parser.add_argument('--mentions_dim', type=int, default=64197)
 parser.add_argument('--hashtags_dim', type=int, default=37489)
 parser.add_argument('--urls_dim', type=int, default=7508)
-parser.add_argument('--emb_dim', type=int, default=128)
-parser.add_argument('--hidden_size', type=int, default=64)
+parser.add_argument('--emb_dim', type=int, default=256)
+parser.add_argument('--hidden_size', type=int, default=256)
 parser.add_argument('--num_feature_size', type=int, default=28)
+parser.add_argument('--meanpooling', default=False, action='store_true')
+parser.add_argument('--maxpooling', default=True, action='store_true')
 
 config = parser.parse_args()
 print(config)
--- model.py
+++ model.py
@@ -2,7 +2,6 @@ import pandas as pd
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import matplotlib.pyplot as plt
 from torch.nn.modules import padding
 
 class neuralNet(nn.Module):
@@ -39,16 +38,17 @@ class MSLELoss(nn.Module):
 class MLPModel(nn.Module):
     def __init__(self, config):
         super(MLPModel, self).__init__()
+        self.config = config
         self.embedding_entities = nn.Embedding(config.entities_dim, config.emb_dim, padding_idx=0)
         # TODO: check padding index, add to other non-numerical features
         # TODO: add configuration 
         self.embedding_mentions = nn.Embedding(config.mentions_dim, config.emb_dim, padding_idx=0)
         self.embedding_hashtags = nn.Embedding(config.hashtags_dim, config.emb_dim, padding_idx=0)
         self.embedding_urls = nn.Embedding(config.urls_dim, config.emb_dim, padding_idx=0)
-        self.embedding_entities.weight.data.uniform_(-0.1, 0.1)
-        self.embedding_mentions.weight.data.uniform_(-0.1, 0.1)
-        self.embedding_hashtags.weight.data.uniform_(-0.1, 0.1)
-        self.embedding_urls.weight.data.uniform_(-0.1, 0.1)
+        # self.embedding_entities.weight.data.uniform_(-0.1, 0.1)
+        # self.embedding_mentions.weight.data.uniform_(-0.1, 0.1)
+        # self.embedding_hashtags.weight.data.uniform_(-0.1, 0.1)
+        # self.embedding_urls.weight.data.uniform_(-0.1, 0.1)
 
         self.linear_entities = nn.Linear(config.emb_dim, config.hidden_size)
         self.linear_mentions = nn.Linear(config.emb_dim, config.hidden_size)
@@ -71,22 +71,23 @@ class MLPModel(nn.Module):
         hashtags_embs = self.embedding_hashtags(hashtags)
         urls_embs = self.embedding_urls(urls)
 
-        hid_entities = self.linear_entities(entities_embs)
-        hid_mentions = self.linear_mentions(mentions_embs)
-        hid_hashtags = self.linear_hashtags(hashtags_embs)
-        hid_urls = self.linear_urls(urls_embs).squeeze()
+        hid_entities = self.linear_entities(self.dropout(entities_embs))
+        hid_mentions = self.linear_mentions(self.dropout(mentions_embs))
+        hid_hashtags = self.linear_hashtags(self.dropout(hashtags_embs))
+        hid_urls = self.linear_urls(self.dropout(urls_embs).squeeze())
 
         # MEANPOOLING
-        # mask_entities = hid_entities!=0
-        # mask_mentions = hid_mentions!=0
-        # mask_hashtags = hid_hashtags!=0
-
-        # hid_entities = hid_entities.sum(dim=1) / mask_entities.sum(dim=1)
-        # hid_mentions = hid_mentions.sum(dim=1) / mask_mentions.sum(dim=1)
-        # hid_hashtags = hid_hashtags.sum(dim=1) / mask_hashtags.sum(dim=1)
-        hid_entities = torch.max(hid_entities, 1)[0]
-        hid_mentions = torch.max(hid_mentions, 1)[0]
-        hid_hashtags = torch.max(hid_hashtags, 1)[0]
+        if self.config.meanpooling:
+            mask_entities = hid_entities!=0
+            mask_mentions = hid_mentions!=0
+            mask_hashtags = hid_hashtags!=0
+            hid_entities = hid_entities.sum(dim=1) / mask_entities.sum(dim=1)
+            hid_mentions = hid_mentions.sum(dim=1) / mask_mentions.sum(dim=1)
+            hid_hashtags = hid_hashtags.sum(dim=1) / mask_hashtags.sum(dim=1)
+        else:
+            hid_entities = torch.max(hid_entities, 1)[0]
+            hid_mentions = torch.max(hid_mentions, 1)[0]
+            hid_hashtags = torch.max(hid_hashtags, 1)[0]
         
 
         # TODO: check size of each layer, check dim of cat
