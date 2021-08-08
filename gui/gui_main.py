e5698efaf149930092358688e04818870f4ddb71
Erasmo1015 <2675325968@qq.com>
2021-08-08 15:01:18 +0800

    update ui

--- gui/gui_main.py
+++ gui/gui_main.py
@@ -11,17 +11,15 @@ import sys
 sys.path.append('../')
 from utils import FeatureDataset
 
+st.set_page_config(page_title='Retweet Prediction Challenge', page_icon=":shark:", layout='centered', initial_sidebar_state='auto')
+
 st.title('COVID-19 Retweet Prediction Challenge')
 
-st.write('Select a model or a combination of models that will be trained using an ensemble method.')
-model_1 = st.checkbox('Model 1')
-model_2 = st.checkbox('Model 2')
-# model_3 = st.checkbox('Model 3')
-# model_4 = st.checkbox('Model 4')
-# model_5 = st.checkbox('Model 5')
-# model_6 = st.checkbox('Model 6')
+# st.write('Select a model or a combination of models that will be trained using an ensemble method.')
+model_1 = st.sidebar.checkbox('Model 1')
+model_2 = st.sidebar.checkbox('Model 2')
 
-ens = st.selectbox('Select an ensemble method',
+ens = st.sidebar.selectbox('Select an ensemble method',
                     ('Regression', 'Average'))
 
 
@@ -31,14 +29,6 @@ if model_1:
     ENSEMBLE_MODELS.append('model1')
 if model_2:
     ENSEMBLE_MODELS.append('model2')
-# if model_3:
-#     ENSEMBLE_MODELS.append('model3')
-# if model_4:
-#     ENSEMBLE_MODELS.append('model4')
-# if model_5:
-#     ENSEMBLE_MODELS.append('model5')
-# if model_6:
-#     ENSEMBLE_MODELS.append('model6')
 
 ### BUILD ENSEMBLE ###
 random.seed(config.seed)
@@ -58,13 +48,15 @@ preds_train_models = pd.DataFrame()
 preds_val_models = pd.DataFrame()
 preds_test_models = pd.DataFrame()
 
-for model in ENSEMBLE_MODELS:
-    # Read saved predictions
-    preds_train_models[model] = pd.read_csv(config.save_dir + model + '_train_pred.csv')['train']
-    preds_val_models[model] = pd.read_csv(config.save_dir + model + '_val_pred.csv')['val']
-    preds_test_models[model] = pd.read_csv(config.save_dir + model + '_test_pred.csv')['test']
+with st.spinner('Wait for it...'):
+    for model in ENSEMBLE_MODELS:
+        # Read saved predictions
+        preds_train_models[model] = pd.read_csv(config.save_dir + model + '_train_pred.csv')['train']
+        preds_val_models[model] = pd.read_csv(config.save_dir + model + '_val_pred.csv')['val']
+        preds_test_models[model] = pd.read_csv(config.save_dir + model + '_test_pred.csv')['test']
 
 if len(preds_train_models) != 0:
+    
     if ens == 'Regression':
         # Regression
         en_model = LinearRegression(fit_intercept=False)
@@ -82,22 +74,29 @@ if len(preds_train_models) != 0:
     val_msle = msle(val_pred, val_dataset.y)
     test_msle = msle(test_pred, test_dataset.y)
 
-    st.subheader('View model results.')
+    st.subheader('View model results:')
     st.write('Validation MSLE: ' + str(val_msle))
     st.write('Test MSLE: ' + str(test_msle))
 
-    st.write('Validation Set')
-    val_df = pd.read_feather('../data/val.feather')
-    target = val_df['0']
-    val_df.drop(labels=['0'], axis=1, inplace=True)
-    val_df.insert(0, 'target', target)
-    val_df = pd.concat([pd.DataFrame(data=val_pred, columns=['predicted']), val_df], axis=1)
-    val_table = st.dataframe(val_df.head(100))
+    with st.echo(code_location='below'):
+        st.subheader('View pre-processed data:')
+        st.write('Validation Set')
+        val_df = pd.read_feather('../data/val.feather')
+        target = val_df['0']
+        val_df.drop(labels=['0'], axis=1, inplace=True)
+        val_df.insert(0, 'target', target)
+        val_df = pd.concat([pd.DataFrame(data=val_pred, columns=['predicted']), val_df], axis=1)
+        val_table = st.dataframe(val_df.head(100))
 
-    st.write('Test Set')
-    test_df = pd.read_feather('../data/test.feather')
-    target = test_df['0']
-    test_df.drop(labels=['0'], axis=1, inplace=True)
-    test_df.insert(0, 'target', target)
-    test_df = pd.concat([pd.DataFrame(data=test_pred, columns=['predicted']), test_df], axis=1)
-    test_table = st.dataframe(test_df.head(100))
+        st.write('Test Set')
+        test_df = pd.read_feather('../data/test.feather')
+        target = test_df['0']
+        test_df.drop(labels=['0'], axis=1, inplace=True)
+        test_df.insert(0, 'target', target)
+        test_df = pd.concat([pd.DataFrame(data=test_pred, columns=['predicted']), test_df], axis=1)
+        test_table = st.dataframe(test_df.head(100))
+        st.subheader('View code:')
+        
+else:
+    st.info('Select a model or a combination of models that will be trained using an ensemble method.')
+    st.image('https://images.unsplash.com/photo-1616469829167-0bd76a80c913?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80')
